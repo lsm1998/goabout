c9d5167b7c9377be50162d9876987ee80fc5c190
lsm <487005831@qq.com>
2022-03-27 19:18:02 +0800

    micro

--- micro/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import mustHttp "github.com/lsm1998/must/http"
-
-func main() {
-	mustHttp.New("")
-}
---
+++ micro/micro-user/handler/greeter.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"context"
+	proto "micro-user/protos"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
