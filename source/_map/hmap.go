109cb3c939113e44785e9271762ca791191b6bcd
lsm <487005831@qq.com>
2022-05-16 23:14:42 +0800

    flags

--- source/_map/hmap.go
+++ source/_map/hmap.go
@@ -9,7 +9,7 @@ type hmap struct {
 	// Note: the format of the hmap is also encoded in cmd/compile/internal/reflectdata/reflect.go.
 	// Make sure this stays in sync with the compiler's definition.
 	count     int // # live cells == size of map.  Must be first (used by len() builtin)
-	-------------
+	flags     uint8
 	B         uint8  // log_2 of # of buckets (can hold up to loadFactor * 2^B items)
 	noverflow uint16 // approximate number of overflow buckets; see incrnoverflow for details
 	hash0     uint32 // hash seed
@@ -25,6 +25,12 @@ type hmap struct {
 type hmap struct {
 	// 元素个数
 	count int
+	// map的状态，有四个标志位，用来指示是否有协程遍历map、是否有协程正在修改map等
+	//iterator     = 1 // there may be an iterator using buckets
+	//oldIterator  = 2 // there may be an iterator using oldbuckets
+	//hashWriting  = 4 // a goroutine is writing to the map
+	//sameSizeGrow = 8 // the current map growth is to a new map of the same size
+	flags uint8
 	// buckets 的对数 log_2
 	B uint8
 	// 指向 buckets 数组，大小为 2^B
