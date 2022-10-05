d3ef68547bdb2124dc1c5988bd67334cd64f4208
lsm <487005831@qq.com>
2022-10-05 17:01:24 +0800

    wmware

--- source/_nil/nil_test.go
+++ source/_nil/nil_test.go
@@ -2,43 +2,22 @@ package _nil
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
-type NilValue struct {
+type Student struct {
 	Name string
 }
 
-func (*NilValue) Say() {
-	fmt.Println("say")
+func (*Student) SayHello() {
+	fmt.Println("hello")
 }
 
-func (n *NilValue) SayName() {
-	fmt.Println(n.Name)
+func (s *Student) Say() {
+	fmt.Println(s.Name)
 }
 
-func Show(val interface{}) {
-	if val == nil {
-		fmt.Println("nil!")
-		return
-	}
-	fmt.Println("show ", val)
-}
-
-func TestNilValue(t *testing.T) {
-	var a1 *int
-	var a2 *float32
-	fmt.Println(a1)
-	fmt.Println(a2)
-
-	Show(nil)
-
-	var info interface{}
-	info = a1
-	Show(info)
-	fmt.Println(reflect.ValueOf(info).IsNil())
-
-	var nv *NilValue
-	nv.Say()
+func TestNil(t *testing.T) {
+	var s1 *Student
+	s1.Say()
 }
