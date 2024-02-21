5652385ced59a70b3ab9867ff41cee565d1641ba
p8s8liushim <p8s8liushim@yujing.live>
2024-02-21 18:44:35 +0800

    ArrayMedian

--- leetcode/base/array_median_test.go
+++ leetcode/base/array_median_test.go
@@ -18,7 +18,8 @@ import (
 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
 */
 
-/**
+/*
+*
 中心思想
 两个数组合并后，根据中位数分为大数部分和小数部分
 则满足 i + j = （m+n）/2
@@ -32,6 +33,39 @@ func TestArrayMedian(t *testing.T) {
 	fmt.Println(ArrayMedian([]int{1, 2}, []int{3, 4}))
 }
 
-func ArrayMedian(arr1, arr2 []int) float32 {
-	return 0
+func ArrayMedian(nums1 []int, nums2 []int) float64 {
+	totalLen := len(nums1) + len(nums2)
+	if totalLen%2 == 0 {
+		return (findKth(nums1, nums2, totalLen/2) + findKth(nums1, nums2, totalLen/2-1)) / 2.0
+	} else {
+		return findKth(nums1, nums2, totalLen/2)
+	}
+}
+
+func findKth(nums1 []int, nums2 []int, k int) float64 {
+	if len(nums1) > len(nums2) {
+		return findKth(nums2, nums1, k)
+	}
+	if len(nums1) == 0 {
+		return float64(nums2[k])
+	}
+	if k == 0 {
+		return float64(min(nums1[0], nums2[0]))
+	}
+
+	p1 := min(k/2, len(nums1)-1)
+	p2 := k - p1 - 1
+
+	if nums1[p1] <= nums2[p2] {
+		return findKth(nums1[p1+1:], nums2, k-p1-1)
+	} else {
+		return findKth(nums1, nums2[p2+1:], k-p2-1)
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
